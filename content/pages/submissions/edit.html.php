f2ac09330c6935aedbd04842c49134eb534a3ca2
Alexander Morland <alek.morland@gmail.com>
2019-03-24 18:19:23 +0100

    Fixed edit sub

--- content/pages/submissions/edit.html.php
+++ content/pages/submissions/edit.html.php
@@ -11,7 +11,7 @@ if ($id == false) {
 	return $this->request->redirect('/');
 }
 
-$sub = Submission::get($id);
+$sub = Submission::get($id)->with('challenge')->with('player');
 if (!$sub) {
 	return $this->request->redirect('/');
 }
@@ -21,13 +21,13 @@ if ($data = $this->request->getPostData()) {
     $data['score'] = (int) $data['score'];
     $data['stars'] = (int) $data['stars'];
     if ($sub->save($data)) {
-        $this->request->redirect('/submissions/list');
+        return $this->request->redirect('/submissions/list');
     }
     dd($sub);
 }
 
 ?>
-<h2>Adding new Submission</h2>
+<h2>Edit Submission for <?=$sub->challenge()->name?> : <?=$sub->player()->name?></h2>
 <form method="POST">
     <fieldset>
         <label>
@@ -89,8 +89,8 @@ if ($data = $this->request->getPostData()) {
         <br />
         <label>
             <span>Played online</span><br />
-            <input type="hidden" name="accepted" value="0" />
-            <input type="checkbox" name="accepted" value="1" <?=$sub->online?'checked="checked"':''?> />
+            <input type="hidden" name="online" value="0" />
+            <input type="checkbox" name="online" value="1" <?=$sub->online?'checked="checked"':''?> />
         </label>
         <br />
         <input type="submit" name="Save">
