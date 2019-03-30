81da30f857fe16b3c509b9d1c2c7b7acd3d39250
Alexander Morland <alek.morland@gmail.com>
2019-03-30 18:22:28 +0100

    bigger icons

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -5,7 +5,7 @@
     <title>Crawl Cosplay Challenge</title>
     <link rel="icon" href="/img/stone_soup_icon-32x32.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
-    <link rel="stylesheet" href="/css/cosplay.css">
+    <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
 </head>
 <body class="page_back">
