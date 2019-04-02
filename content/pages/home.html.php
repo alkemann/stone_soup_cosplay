a53c58339ec5d880fb02f58de6648d3ab706f630
alkemann <alek.morland@gmail.com>
2019-04-02 22:38:31 +0200

    Link to reddit thread also on home page on set name

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -8,13 +8,13 @@ if ($active) :
 	$challenges_in_set = Challenge::findAsArray(['setnr' => $set, 'draft' => 0], ['order' => '`week` ASC']);
 	$weeks = sizeof($challenges_in_set);
 ?>
-<h2><?php if ($active->icon): ?><img src="<?=$active->icon?>" class="head-icon" height="24px" /> <?php endif; ?>Set <?=$active->setnr?> Week <?=$active->week?> : <?=$active->name?></h2>
+<h2><?php if ($active->icon): ?><img src="<?=$active->icon?>" class="head-icon" height="24px" /> <?php endif; ?><a href="<?=$cha->reddit?>">Set <?=$active->setnr?> Week <?=$active->week?> : <?=$active->name?></a></h2>
 <p style="font-style: italic; color: #777;"><?=$active->description?></p>
 <table class="table_for_layout">
 	<tr><th>Species</th><th>Background<th>Gods</th></tr>
 	<tr><td><?=$active->species?></td><td><?=$active->background?><td><?=$active->gods?></td></tr>
 </table>
-<p>See current <a href="/challenges/details?id=<?=$active->id?>">challenge details here</a> or <a href="/submissions/submit">Submit a run</a>.</p>
+<p>See current <a href="/challenges/details?id=<?=$active->id?>">challenge details here</a> or <a href="/submissions/submit">Submit a run</a> or <a href="<?=$active->reddit?>">discuss it on reddit</a>.</p>
 <hr />
 <h2>Set <?=$active->setnr?> Scoreboard</h2>
 <ol>
@@ -28,7 +28,7 @@ if ($active) :
 			<!-- <th>id</th> -->
 			<th>Player</th>
 			<th>Total</th>
-			<?php 
+			<?php
 			foreach ($challenges_in_set as $c) {
 				echo '<th>';
 				if ($cha->icon) echo '<img src="'.$c->icon.'" style="height: 1.5em" /> ';
@@ -37,12 +37,12 @@ if ($active) :
 			?>
 		</tr>
 	</thead>
-	<tbody><?php 
+	<tbody><?php
 	foreach ($scores as $i => $row) : ?>
 		<tr class="<?=$i%2==0?'odd':'even'?>">
 			<td><?=$row['player']?></td>
 			<td><?=$row['total']?> <?=$row['stars']?>*</td>
-			<?php 
+			<?php
 
 			foreach ($challenges_in_set as $c) {
 				$week = $row['week'][$c->id];
@@ -51,15 +51,15 @@ if ($active) :
 					continue;
 				}
 				$sc = $week['score']; $st = "";
-				for ($i=0; $i < (int) $week['stars'] ; $i++) { 
+				for ($i=0; $i < (int) $week['stars'] ; $i++) {
 					$st .= '*';
-				} 
-				for ($i=0; $i < 2 - (int) $week['stars'] ; $i++) { 
+				}
+				for ($i=0; $i < 2 - (int) $week['stars'] ; $i++) {
 					$st .= '_';
 				}
 				echo "<td>{$sc} {$st}</td>";
 			}
-			for ($i=0; $i < $weeks - sizeof($row['week']); $i++) { 
+			for ($i=0; $i < $weeks - sizeof($row['week']); $i++) {
 				echo "<td>&nbsp;</td>";
 			}
 			?>
