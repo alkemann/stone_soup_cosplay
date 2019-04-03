02e0f1da36c0835bec40ea644e9ab98d07d5f4fe
Alexander Morland <alek.morland@gmail.com>
2019-04-03 16:33:58 +0200

    For #26 centered auto margin layout

--- content/layouts/default/foot.html.php
+++ content/layouts/default/foot.html.php
@@ -1,4 +1,6 @@
+		<br />
 		<hr />
+		<br />
 		<footer>Copyright .. nah  |  <a href="https://github.com/alkemann/stone_soup_cosplay/issues/new" target="_blank">Send bug report or feature request</a></footer>
 		</div>
 	</div>
--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -17,6 +17,7 @@
             <h1><a href="/">Crawl Cosplay Challenge</a></h1>
             <div class="fineprint">Come chat with us on the offical CCC Discord server: <a href="https://discord.gg/ZQ4kk6n" target="_blank">https://discord.gg/ZQ4kk6n</a></div>
         </div>
+        <br />
         <hr />
         <div class="content">
 
