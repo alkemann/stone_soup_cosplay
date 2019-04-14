5fbcd0e4dfeb2b2097ec6d9539ee6ddba7c5de9e
alkemann <alek.morland@gmail.com>
2019-04-14 10:42:04 +0200

    Fixes #33 adds an about page

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -1,16 +1,17 @@
 <div class="page information">
     <div class="toplinks">
         <a href="/">Home</a>
+        <a href="/">About</a>
         | <a href="/history">History</a>
     <?php if ($this->request->session('admin')) : ?>
         | Subs <a href="/submissions/list">Official</a> <a href="/submissions/moderate">Moderate</a> <a href="/submissions/add">New</a>
         | Players <a href="/players/list">List</a> <a href="/players/add">New</a>
     	| Challenges <a href="/challenges/list">List</a> <a href="/challenges/add">New</a>
         | <a href="/logout">Logout</a>
-    
+
 	<?php else : ?>
 	   | <a href="/admin">Admin</a>
-	<?php endif; ?>		
+	<?php endif; ?>
     </div>
     <div class="page_content">
         <div class="heading">
