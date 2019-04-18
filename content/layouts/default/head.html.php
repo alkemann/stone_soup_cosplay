dfe118a9b2abc1025e79457e4664700c4c16e864
kitchen-ace <68k@quikphix.org>
2019-04-18 18:52:30 -0400

    Change page icon to mask of the dragon

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Crawl Cosplay Challenge</title>
-    <link rel="icon" href="/img/stone_soup_icon-32x32.png" type="image/png">
+    <link rel="icon" href="/img/urand_dragonmask.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
