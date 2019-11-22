5b196e7d7813702e982cb668e1357cdb0d4d4def
kitchen-ace <47063032+kitchen-ace@users.noreply.github.com>
2019-11-22 05:12:22 -0500

    Link MalcolmRose's video on the main page
    Quick hack to insert a link on the main page. If this becomes a regular thing, we can do it properly.

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -23,6 +23,9 @@ if ($active) :
 
 <hr />
 
+<p style="color:cyan;">Watch Malcolm Rose play the Pandemonium Lord challenge: <a href="https://www.twitch.tv/videos/511158172">https://www.twitch.tv/videos/511158172</a></p>
+<p style="color:cyan;">Interested in having your own streams on the Cosplay Challenge website? Let us know in the comments on reddit!</p>
+
 <h2>Scoreboard for Set <?=$e($active->setnr)?></h2>
 <table class="set-list">
 <?php 
