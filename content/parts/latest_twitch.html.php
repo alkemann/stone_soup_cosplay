fe06c4272c6c4193ca3f520bb76856834f9c6530
RoGGa <robert_gagne@yahoo.com>
2019-11-28 17:42:35 -0500

    change twitch link to youtube

--- content/parts/latest_twitch.html.php
+++ content/parts/latest_twitch.html.php
@@ -1,5 +1,5 @@
 <p style="color:cyan;">
-    Watch Malcolm Rose play the Pandemonium Lord challenge: <a href="https://www.twitch.tv/videos/511158172">https://www.twitch.tv/videos/511158172</a>
+    Watch Malcolm Rose play the Pandemonium Lord challenge: <a href="https://youtu.be/NevFoRhbAgU">https://youtu.be/NevFoRhbAgU</a>
 </p>
 <p style="color:cyan;">
     Interested in having your own streams on the Cosplay Challenge website? Let us know in the comments on reddit!
