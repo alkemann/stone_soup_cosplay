9739ad4f36eb736343d6b08ae11b6df6f80993c8
RoGGa <robert_gagne@yahoo.com>
2019-11-29 15:15:48 -0500

    fix spelling error: offical

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -17,7 +17,7 @@
     <div class="page_content">
         <div class="heading">
             <h1><a href="/">Crawl Cosplay Challenge</a></h1>
-            <div class="fineprint">Come chat with us on the offical CCC Discord server: <a href="https://discord.gg/ZQ4kk6n" target="_blank">https://discord.gg/ZQ4kk6n</a></div>
+            <div class="fineprint">Come chat with us on the official CCC Discord server: <a href="https://discord.gg/ZQ4kk6n" target="_blank">https://discord.gg/ZQ4kk6n</a></div>
         </div>
         <br />
         <hr />
