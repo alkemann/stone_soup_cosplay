3a1dd92ae46ccc24156d2ff73caca15ff417a397
RoGGa <robert_gagne@yahoo.com>
2020-03-24 15:43:49 -0400

    Updated Malcolm's link to a playlist
    https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV

--- content/parts/latest_twitch.html.php
+++ content/parts/latest_twitch.html.php
@@ -1,5 +1,5 @@
 <p style="color:cyan;">
-    Watch Malcolm Rose play the Pandemonium Lord challenge: <a href="https://youtu.be/NevFoRhbAgU">https://youtu.be/NevFoRhbAgU</a>
+    Watch Malcolm Rose play some Cosplay challenges: <a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV">https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV
 </p>
 <p style="color:cyan;">
     Interested in having your own streams on the Cosplay Challenge website? Let us know in the comments on reddit!
