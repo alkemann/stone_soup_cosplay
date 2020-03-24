80a61fac9242c24579cd8fa0f82465ec6d2f1425
RoGGa <robert_gagne@yahoo.com>
2020-03-24 15:48:06 -0400

    Update latest_twitch.html.php

--- content/parts/latest_twitch.html.php
+++ content/parts/latest_twitch.html.php
@@ -1,5 +1,5 @@
 <p style="color:cyan;">
-    Watch Malcolm Rose play some Cosplay challenges: <a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV">https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV
+    Watch Malcolm Rose play some Cosplay challenges: <a href="https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV">https://www.youtube.com/playlist?list=PLDaRV3E1eciYScSTw2RnztSrkVLNpbBAV</a>
 </p>
 <p style="color:cyan;">
     Interested in having your own streams on the Cosplay Challenge website? Let us know in the comments on reddit!
