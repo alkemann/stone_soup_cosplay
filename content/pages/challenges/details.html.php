5dd6ced1e34a7fe9a6cf9f95bb29c3f91c29a961
RoGGa <robert_gagne@yahoo.com>
2020-03-28 13:30:58 -0400

    added numbering to Conducts and Bonuses

--- content/pages/challenges/details.html.php
+++ content/pages/challenges/details.html.php
@@ -94,16 +94,16 @@ $this->setData("meta", ['filename' => $cha->icon]);
 
 <h3>Cosplay conduct points</h3>
 <dl>
-	<dt><?=$e($cha->conduct_name_1)?></dt><dd><?=$em($cha->conduct_1)?></dd>
-	<dt><?=$e($cha->conduct_name_2)?></dt><dd><?=$em($cha->conduct_2)?></dd>
-	<dt><?=$e($cha->conduct_name_3)?></dt><dd><?=$em($cha->conduct_3)?></dd>
+	<dt>1. <?=$e($cha->conduct_name_1)?></dt><dd><?=$em($cha->conduct_1)?></dd>
+	<dt>2. <?=$e($cha->conduct_name_2)?></dt><dd><?=$em($cha->conduct_2)?></dd>
+	<dt>3. <?=$e($cha->conduct_name_3)?></dt><dd><?=$em($cha->conduct_3)?></dd>
 </dl>
 <p class="info">Conducts are worth +5 points each, to a maximum of half your score from milestones, rounded down. (So if you achieve 4 milestones (20 points) you can earn up to 10 points from conduct bonuses.) Please indicate which conducts you qualify for when you post your morgue. Small mistakes in following conducts will usually be forgiven.</p>
 
 <h3>Bonus challenges</h3>
 <dl>
-	<dt><?=$e($cha->bonus_name_1)?></dt><dd><?=$em($cha->bonus_1)?></dd>
-	<dt><?=$e($cha->bonus_name_2)?></dt><dd><?=$em($cha->bonus_2)?></dd>
+	<dt>1. <?=$e($cha->bonus_name_1)?></dt><dd><?=$em($cha->bonus_1)?></dd>
+	<dt>2. <?=$e($cha->bonus_name_2)?></dt><dd><?=$em($cha->bonus_2)?></dd>
 </dl>
 <p class="info">Bonus challenges are worth one star each, similar to banners in Crawl tournaments. Please indicate challenges that you qualify for. Small mistakes will usually be forgiven.</p>
 
