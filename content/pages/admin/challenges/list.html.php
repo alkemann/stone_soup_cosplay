53e4de133d9544b0ae9b7e66c7ee5f22824ab28c
kitchen-ace <47063032+kitchen-ace@users.noreply.github.com>
2020-08-20 20:01:23 -0400

    Extend list of challenges in admin list page
    90 is enough to cover all the remaining challenges.

--- content/pages/admin/challenges/list.html.php
+++ content/pages/admin/challenges/list.html.php
@@ -17,7 +17,7 @@ use app\models\Challenge;
 	<tbody>
 	<?php
 		// Supports limit and offset as first and second params for pagination, first param includes drafts
-		$challenges = Challenge::findBySets(true, 75);
+		$challenges = Challenge::findBySets(true, 90);
 		$r = 0;
 		foreach ($challenges as $c) :
 	?>
