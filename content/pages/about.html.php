d30dd71a0155826d9ccfdfdc84788bd7c56fc2c6
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2023-05-27 12:11:26 -0400

    Changed Fridays to Mondays

--- content/pages/about.html.php
+++ content/pages/about.html.php
@@ -4,7 +4,7 @@
 
 <p>Each week will feature a character for you to play, with one or more options for starting species and background, as well as which god(s) you must worship. These choices are all mandatory. Unless otherwise specified, you must worship one of the options for gods before entering any multi-level branch besides the dungeon (e.g. Lair, Orc, Depths), and you can't do anything to lose your religion, including abandoning your god.</p>
 
-<p>New challenges are posted Fridays. Challenges will not run when official DCSS tournaments or other large tournaments/challenges (e.g <a href="http://csclub.uwaterloo.ca/~ebering/crawl/csdc/index.html" target="_blank">CSDC</a>) are going on. 
+<p>New challenges are posted Mondays. Challenges will not run when official DCSS tournaments or other large tournaments/challenges (e.g <a href="http://csclub.uwaterloo.ca/~ebering/crawl/csdc/index.html" target="_blank">CSDC</a>) are going on. 
 
 <p>Challenges can be played in whatever version including trunk and forks. You can play online or offline, and can make as many attempts as you want.</p>
 
