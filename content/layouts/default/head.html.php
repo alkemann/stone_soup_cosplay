cc0da831a0addeb78f792f78bfbd9e9cf16808d4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2023-12-15 22:14:15 -0500

    Update head.html.php to use santa_hat.png

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title><?=$page_title ?? 'Crawl Cosplay Challenge'?></title>
-    <link rel="icon" href="/img/urand_dragonmask.png" type="image/png">
+    <link rel="icon" href="/img/santa_hat.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
