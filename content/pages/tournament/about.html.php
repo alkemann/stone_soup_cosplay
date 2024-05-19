b52b5ba2b3dec5d351fe3ce2561b53aee4ae0bb4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-19 14:41:02 -0400

    Create tournament about.html.php

---
+++ content/pages/tournament/about.html.php
@@ -0,0 +1,52 @@
+<h2>About Crawl Cosplay Trunk Tournament</h2>
+
+<p>The Crawl Cosplay Trunk Tournament is a 4-week tournament with 4 challenges based on the weekly <a href="http://cosplay.kelbi.org/" target="_blank">Crawl Cosplay Challenge</a> series for <a href="http://crawl.develz.org/" target="_blank">Dungeon Crawl Stone Soup</a>. Each of the challenges have been selected to highlight major Trunk changes since the previous stable version.</p> 
+
+<p>Each week will feature a DCSS Unique character for you to play, with one or more options for starting species and background, as well as which god you must worship. These are all mandatory and you must worship your god before entering any multi-level branch besides the dungeon (e.g. Lair, Orc, Depths). Also, you can't do anything to lose your religion, including abandoning your god. The goal is not to emulate the unique's behaviour exactly, but to get a good starting point on playing games with interesting requirements.</p>
+
+<p>The next tournament challenge will be posted Friday at 0:00 UTC.</p>
+
+<h3>How to Enter</h3>
+
+<p>The current challenge details page has a "Submit a run" link. For your 1st submission, paste a link to your online morgue, and enter your playername and score in the comments. 
+Submissions will be approved by a moderator, who will verify your morgue and your score, and create a username for your future submissions.</p>
+<p><b>Submitted runs must be played in trunk on one of the official online server.</b> You can make as many attempts as you want. </p>
+
+<h3>Changing your player sprite to a Unique.</h3>
+<p>If you'd like to change the player doll to the Unique's tile, add the following to your RC file:</p>
+<p> tile_player_tile = tile:MONS_WIGLAF</p>
+
+<h3>Rules and Scoring</h3>
+
+<h4>Milestones</h4> 
+<p>These remain the same from one week to the next, and are the main way of scoring points. Each milestone earns you 5 points, for a total of 35 if you win the game, and can be done in any order. You can only earn points from each milestone once per game (entering Lair and then Orc doesn't get you 10 points).</p>
+
+<ul><li>Reach XL3.</li>
+<li>Enter Lair, Orc, or Depths.</li>
+<li>Reach the last floor of Lair, Orc, or the Dungeon.</li>
+<li>Collect your first rune.</li>
+<li>Find the entrance to Zot. (Just using magic mapping doesn't count; you must actually have the entrance in LOS.)</li>
+<li>Collect three runes.</li>
+<li>Win the game.</li></ul>
+
+<h4>Conducts</h4>
+<p>Each week will have three conducts for you to follow, that vary with every challenge. Following or completing each conduct is worth 5 points, to a maximum of half your score from milestones, rounded down. (So if you achieve 3 milestones (15 points) you can earn up to 7 points from conduct bonuses, for a total of 22 points.)</p>
+
+<h4>Bonus Challenges:</h4> 
+<p>Each of the 4 challenges will also have two <em>optional</em> bonus challenges, that are usually difficult or unconventional. Completing a bonus challenge doesn't affect your score, but gets you a <strong>star</strong>, a small trophy to recognize your skill. Each week has one star that requires you to win the game, and another that doesn't. You don't need to follow any of the cosplay conducts to earn stars.</p>
+
+<h3>Credits</h3>
+
+<p>The Crawl Cosplay Trunk Tournament is created and run by <a href="https://www.reddit.com/user/RoGGa_69/" target="_blank">RoGGa</a> with the help from some of the Crawl Cosplay Challenge regulars.</p>
+
+<p>This website was developed by <a href="https://github.com/alkemann" target="_blank">Alkemann</a>, with assistance from RoGGa and kitchen_ace. CSS by kitchen_ace and Alkemann, loosely based on the one from the <a href="https://github.com/zxc23/dcss-scoreboard" target="_blank">DCSS Scoreboard</a>, with images from DCSS.</p>
+
+<p>A big thank you to floraline for hosting CCC and the torunament on the old CKO server.
+
+</p><h4>Inspiration and influences:</h4>
+
+<ul><li>Casual League challenges by <a href="http://crawl.akrasiac.org/scoring/players/miek.html" target="_blank">triorph aka miek</a></li>
+<li>Scrub League and other challenges by <a href="http://crawl.akrasiac.org/scoring/players/vajrapani.html" target="_blank">VajRapani</a></li>
+<li>konebred's <a href="https://www.reddit.com/r/dcss/comments/8e7mi2/weekly_challenge_reboot_attempt_1/" target="_blank">attempt</a> at a similar challenge series</li></ul>
+
+<p>Special thanks to all CCC participants, and DCSS devs!</p>
