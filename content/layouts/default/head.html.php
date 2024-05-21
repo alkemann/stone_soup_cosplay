6f57ccff1e2ee84f5b8feee6f74beb72703e62c3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-21 17:17:10 -0400

    Update head.html.php
    changed tab icon to: feat-dg_entrance.png

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title><?=$page_title ?? 'Crawl Cosplay Challenge'?></title>
-    <link rel="icon" href="/img/urand_dragonmask.png" type="image/png">
+    <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
