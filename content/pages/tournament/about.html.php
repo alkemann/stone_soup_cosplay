b7ebde76a634e85284e7bb0355f668649be381af
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-21 09:10:14 -0400

    Update about.html.php

--- content/pages/tournament/about.html.php
+++ content/pages/tournament/about.html.php
@@ -15,13 +15,14 @@
 <p>The current challenge's details page has a "Submit a run" link.
  For those who haven't participated in CCC or CCTT before, just paste a link to your online morgue, and enter your playername and score in the comments. 
  Submissions will be approved by a moderator, who will verify your morgue and your score, and create a username for your future submissions.</p>
+
 <h3>Restrictions for tournament play</h3>
 <p><b>- Submitted runs must be played in trunk on one of the <a href="https://crawl.develz.org/play.htm" target="_blank"> official online server.</a></b></p>
 <p><b>- Seeded games are NOT accepted.</b></p>
 <p>- You can make as many attempts as you want.</p>
-<p>- Morgues must be submitted while a challenge is active. No late submission!</p>
+<p>- Morgues must be submitted while a challenge is active. No late submissions!</p>
 
-<h3>Changing your player sprite to a Unique.</h3>
+<h3>Changing your player sprite to a Unique's doll.</h3>
 <p>If you'd like to change the player doll to the Unique's tile, add the following to your RC file:</p>
 <p> tile_player_tile = tile:MONS_WIGLAF</p>
 
