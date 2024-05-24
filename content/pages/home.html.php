1ebdb1b710c74fe4d542533b6daa49fe6954a1cf
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-24 05:22:55 -0400

    Update /home.html.php
    fixed grammar

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -96,7 +96,7 @@ if ($active) :
 
 <?php else : // no active challengs
 	echo "<h3>There is currently no active challenge for the weekly Crawl Cosplay Challenge (CCC) series.</h3>";
-	echo '<p>For your info, you can still submit a run/morgue for <a href="https://cosplay.kelbi.org/history">any CCC challenges</a>.</p>';
+	echo '<p>For your info, you can still submit a run/morgue for <a href="https://cosplay.kelbi.org/history">any CCC challenge</a>.</p>';
 
 	$tournament_challenge = Challenge::tournamentActive();
 	if ($tournament_challenge) {
