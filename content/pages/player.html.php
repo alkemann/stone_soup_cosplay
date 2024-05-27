23e5ddf594075a725870dface913ce19bbf62d06
Alexander Morland <alexander.morland@tv2.no>
2024-05-27 17:01:49 +0200

    Refactor player.html.php to separate tournament and regular submissions

--- content/pages/player.html.php
+++ content/pages/player.html.php
@@ -17,26 +17,104 @@ if (!$p) {
 }
 
 $submissions = $p->submissions();
-// usort($submissions, function(Submission $a, Submission $b) : int {
-//     $ac = $a->challenge();
-//     $bc = $b->challenge();
-//     $as = (int) $ac->setnr;
-//     $bs = (int) $bc->setnr;
-//     $aw = (int) $ac->week;
-//     $bw = (int) $bc->week;
-//     if ($as == $bs) {
-//         return $bw <=> $aw;
-//     }
-//     return $bs <=> $as;
-// });
+$tournament_submissions = [];
+$normal_submissions = [];
 
-// @TODO if no subs
+array_reduce($submissions, function($carry, Submission $s) use (&$tournament_submissions, &$normal_submissions) {
+    if ($s->challenge()->setnr > 30) {
+        $tournament_submissions[] = $s;
+    } else {
+        $normal_submissions[] = $s;
+    }
+}, null);
+
+
+if ($tournament_submissions) :
+
+
+    $board = [];
+    $first_set = 999;
+    $last_set = 0;
+    foreach ($tournament_submissions as $sub) {
+        if ($sub->isScoring() === false) {
+            continue;
+        }
+        $cha = $sub->challenge();
+        $set = (int) $cha->setnr;
+        $week = (int) $cha->week;
+        if ($sub->hs === false || $week > 5) {
+            continue;
+        }
+        $first_set = $first_set < $set ? $first_set : $set;
+        $last_set = $last_set > $set ? $last_set : $set;
+        $board[$set][$week] = $sub;
+        if (!isset($board[$set]['total'])) $board[$set]['total'] = 0;
+        if (!isset($board[$set]['stars'])) $board[$set]['stars'] = 0;
+        $board[$set]['total'] += $sub->score;
+        $board[$set]['stars'] += $sub->stars;
+    }
+    for ($set_key=$last_set; $set_key >= $first_set; $set_key--) {
+        if (isset($board[$set_key]) == false) {
+            $board[$set_key]['total'] = 0;
+            $board[$set_key]['stars'] = 0;
+        }
+    }
+
+?>
+<h2>Submissions by <?=$e($p->name)?></h2>
+
+<h3>Crawl Cosplay Trynk Tournaments</h3>
+<table class="bordered player-list">
+    <thead>
+        <tr>
+            <th>&nbsp;</th>
+            <th>Total</th>
+            <th>Wk 1</th>
+            <th>Wk 2</th>
+            <th>Wk 3</th>
+            <th>Wk 4</th>
+            <th>Wk 5</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        $r = 0;
+        for ($set_key=$last_set; $set_key >= $first_set; $set_key--) : ?>
+        <tr class="<?=$r++%2==0?'odd':'even'?>">
+            <th>Set <?=$set_key?></th>
+            <td><?=$board[$set_key]['total']?> <?=$board[$set_key]['stars']?><span class="star">&#9733;</span></td>
+        <?php for ($i=1; $i <= 5; $i++) : ?>
+            <?php
+                $sub = $board[$set_key][$i] ?? false;
+                if ($sub === false) {
+                    echo '<td>&nbsp;</td>';
+                    continue;
+                }?>
+            <td>
+                <?php if ($sub->morgue_url) : ?><a href="<?=$e($sub->morgue_url)?>" target="_blank"><?php endif; ?>
+                <?php
+                echo $sub->score;
+                for ($stars=0; $stars < (int) $sub->stars ; $stars++) {
+                    echo '<span class="star">&#9733;</span>';
+                }
+                ?>
+                <?php if ($sub->morgue_url) : ?></a><?php endif; ?>
+            </td>
+        <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>
+    </tbody>
+</table>
+
+<br/>
+<?php
+endif; // tournament submissions
 
 $has_non_scoring = false;
 $board = [];
 $first_set = 999;
 $last_set = 0;
-foreach ($submissions as $sub) {
+foreach ($normal_submissions as $sub) {
     if ($sub->isScoring() === false) {
         continue;
     }
@@ -61,9 +139,8 @@ for ($set_key=$last_set; $set_key >= $first_set; $set_key--) {
         $board[$set_key]['stars'] = 0;
     }
 }
-
 ?>
-<h2>Submissions by <?=$e($p->name)?></h2>
+<h3>Regular Crawl Cosplay Challenges</h3>
 
 
 <table class="bordered player-list">
