0be25b66e4247f438f6bb3e13c9d185652747be1
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-28 13:36:53 -0400

    Create /academy/index.html.php

---
+++ content/pages/academy/index.html.php
@@ -0,0 +1,16 @@
+<h2>Welcome green Crawler!</h2>
+<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">Want to improve your gameplay while having fun doing it? 
+With the Crawl Cosplay Academy (CCA), you'll play easier combos of different playstyles as you learn to survive longer. And 
+hopefully win many games sooner than later, while playing characters based on 
+<a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
+<h4>Ready?</h4>
+Want to know more before jumping in? Read the <a href="/about">About</a> section.
+<h4>Set...</h4>
+If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed above since they are considered to be the
+<b><i>easier</i></b> combos of the easiest category. 
+<h4>GO!</h4>
+Once you have completed one of your better games, <a href="/submit">Submit your Morgue</a> to gain access to your porfolio</a>. 
+Also, feel free to submit your old morgues that used any of the Unique's specific combos.
+<br /><br />
+<h3>Need more help?</h3>
+<p>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Crawl Cosplay Discord server</a>.
