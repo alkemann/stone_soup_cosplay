5694d994e079ce278c4b416c65a341f584bb7721
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-28 13:22:43 -0400

    Create /academy/about.html.php

---
+++ content/pages/academy/about.html.php
@@ -0,0 +1,67 @@
+<h2 style="color:rgb(69, 136, 5);">About Cosplay Academy</h2>
+The Crawl CosPlay Academy (CCA) is intended as a prequel to the weekly 
+<a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC)...and yet, 
+on its own, it is so much more.
+As a prequel, it's for players who don't feel they have the skills necessary  to compete in the Crawl Cosplay Challenge at a level
+that isn't just beginner.
+In general though, the audience is <abbr title="Dungeon Craw: Stone Soup">DCSS</abbr> players who are fairly new to the game or 
+haven't won at least a game with each of the four playstyles.
+
+<h3>Process</h3>
+After you complete a good game (with either a win or <abbr title="Yet Another Sad Death">YASD</abbr>), you <a href="/submit">Submit your Morgue</a> to track your progress.
+If you did not win the game, you get assigned a ghost XL title*:
+<br><br>
+<table>
+  <tr><th>XL</th><th>Title</th></tr>
+  <tr><td>&nbsp3</td><td>Weakling</td></tr>
+  <tr><td>&nbsp6</td><td>Amateur</td></tr>
+  <tr><td>&nbsp9</td><td>Novice</td></tr>
+  <tr><td>12</td><td>Journeyman</td></tr>
+  <tr><td>15</td><td>Adept</td></tr>
+  <tr><td>18</td><td>Veteran</td></tr>
+  <tr><td>21</td><td>Master</td></tr>
+  <tr><td>24+</td><td>Legendary</td></tr>
+</table>
+<br />
+<p>There are 4 playstyles with 3 uniques each:
+  <a href="/melee">Melee Weapons</a>, <a href="/ranged">Ranged Weapons</a>, 
+  <a href="hybrid">Hybrid</a>, and    <a href="magic">Magic</a>. </p>
+
+<p>When you do win, you'll be awarded the Bronze medal for that playstyle.
+  With a 2nd win from the different playstyle, you'll be awarded a Silver medal.
+  And again a 3rd win in a 3rd category will grant you the Gold medal.</p>
+  Finally, when you have a win in each of the 4 playstyle, you'll be fully Certified by the CosPlay Academy.
+
+<p>Also, once you have a win, you'll be added to the <a href="/certified">Honour Roll</a> allowing you to see also the accomplishment of other crawlers.</p>
+<p>Here's an example of a detailed player profile:</p>
+<br />
+<table>
+  <tr><th colspan="2"> Playstyle:</th><th colspan="3">Melee</th><th colspan="3">Ranged</th><th colspan="3">Hybrid</th><th colspan="3">Magic</th></tr>
+  <tr><th>Title</th><th>Level</th>
+      <th><a href="/uniques/snorg" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></th>
+      <th><a href="/uniques/asterion" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></th>
+      <th><a href="/uniques/saint-roka" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></th>
+      <th><a href="/uniques/nessos" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a></th>
+      <th><a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a></th>
+      <th><a href="/uniques/joseph" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a></th>
+      <th><a href="/uniques/grum" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a></th>
+      <th><a href="/uniques/ilsuiw" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a></th>
+      <th><a href="/uniques/jory" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></th>
+      <th><a href="/uniques/tiamat" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a></th>
+      <th><a href="/uniques/fannar" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a></th>
+      <th><a href="/uniques/josephine" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a> </th></tr>
+  <tr><td>Medal</td><td>WIN</td><td>&nbsp&nbsp &#10004</td><td></td><td></td><td></td><td>&nbsp&nbsp &#10004</td></tr></tr>
+  <tr><td>Legendary</td><td>24-27</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>&nbsp&nbsp &#10004</td></tr>
+  <tr><td>Master</td><td>21-23</td><td></td><td>&nbsp&nbsp &#10004</td></tr>
+  <tr><td>Veteran</td><td>18-20</td></tr>
+  <tr><td>Adept</td><td>15-17</td></tr>
+  <tr><td>Journeyman</td><td>12-14</td></tr>
+  <tr><td>Novice</td><td>9-11</td></tr>
+  <tr><td>Amateur</td><td>6-8</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>&nbsp&nbsp &#10004</td></tr>
+  <tr><td>Weakling</td><td>3-5</td></tr>
+  <tr><td>-</td><td>1-2</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>&nbsp&nbsp &#10004</td></tr>
+</table>
+Once you are certified, you'll likely feel ready to compete at an intermediate to advanced level in the weekly 
+<a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.
+<p>_______<br />
+* taken from: <a href="https://github.com/crawl/crawl/blob/master/crawl-ref/source/describe.cc#L4661">https://github.com/crawl/crawl/blob/master/crawl-ref/source/describe.cc#L4661</a></p>
