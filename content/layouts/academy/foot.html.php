ee7ebb347ea820418dc4e04d37bb758dcd18df96
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-28 13:56:55 -0400

    Create /academy/foot.html.php

---
+++ content/layouts/academy/foot.html.php
@@ -0,0 +1,11 @@
+		<br />
+		<img src="/img/HR-right.png"><br />
+		<br />
+		<footer>Copyleft  |  <a href="https://github.com/alkemann/stone_soup_cosplay/issues/new" target="_blank">Send bug report or feature request</a></footer>
+		</div>
+	</div>
+</div>
+</div>
+<div id="page_footer">&nbsp;</div>
+</body>
+</html>
