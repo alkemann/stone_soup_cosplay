c12dbddaa03d0fa6eb4c26eb0169361513650073
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-28 20:21:43 -0400

    Update index.html.php

--- content/pages/academy/index.html.php
+++ content/pages/academy/index.html.php
@@ -6,8 +6,17 @@ hopefully win many games sooner than later, while playing characters based on
 <h4>Ready?</h4>
 Want to know more before jumping in? Read the <a href="/about">About</a> section.
 <h4>Set...</h4>
-If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed above since they are considered to be the
-<b><i>easier</i></b> combos of the easiest category. 
+If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed below since they are considered to be the
+<b><i>easier</i></b> combos of the easiest category.
+<br>
+<p><b>Melee</b></p>
+<p><a href="/academy/challengedetails?id=251" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></p>
+<p><a href="/academy/challengedetails?id=291" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></p>
+<p><a href="/academy/challengedetails?id=171" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></p>
+<p><a href="/ranged" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a>
+<p><a href="/ranged" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a><a href="/ranged" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a>&nbsp
+<p><a href="/hybrid" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a><a href="/hybrid" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a><a href="/hybrid" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a>&nbsp
+<p><a href="/magic" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a><a href="/magic" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a><a href="/magic" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a>
 <h4>GO!</h4>
 Once you have completed one of your better games, <a href="/submit">Submit your Morgue</a> to gain access to your porfolio</a>. 
 Also, feel free to submit your old morgues that used any of the Unique's specific combos.
