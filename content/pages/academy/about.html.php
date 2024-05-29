06666621242de083981cc58d93fead6e2f1913c1
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-29 13:46:02 -0400

    Update /academy/about.html.php

--- content/pages/academy/about.html.php
+++ content/pages/academy/about.html.php
@@ -1,6 +1,6 @@
 <?php $this->layout = 'academy'; ?>
-<h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Academy</h2>
-The <b>Crawl Cosplay Academy</b> (CCA) is intended as a prequel to the weekly <a href="/" target="_blank">Crawl Cosplay Challenge</a> (CCC)...and yet, 
+<h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Academy (CCA)</h2>
+The <b>Crawl Cosplay Academy</b> is intended as a prequel to the weekly <a href="/" target="_blank">Crawl Cosplay Challenge</a> (CCC)...and yet, 
 on its own, it is so much more.
 <p>As a prequel, it's for players who don't feel they have the skills necessary to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.</p>
 <p>In general though, the audience is <abbr title="Dungeon Craw: Stone Soup">DCSS</abbr> players who are fairly new to the game or 
@@ -14,22 +14,28 @@ After you complete a good game (with either a win or <abbr title="Yet Another Sa
 <br />
 <table>
   <tr><th colspan="2"> Playstyle:</th><th colspan="3">Melee</th><th colspan="3">Ranged</th><th colspan="3">Hybrid</th><th colspan="3">Magic</th></tr>
-  <tr><td></td>
-      <td><a href="/uniques/snorg" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></td>
-      <td><a href="/uniques/asterion" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></td>
-      <td><a href="/uniques/saint-roka" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></td>
-      <td><a href="/uniques/nessos" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a></td>
-      <td><a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a></td>
-      <td><a href="/uniques/joseph" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a></td>
-      <td><a href="/uniques/grum" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a></td>
-      <td><a href="/uniques/ilsuiw" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a></td>
-      <td><a href="/uniques/jory" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></td>
-      <td><a href="/uniques/tiamat" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a></td>
-      <td><a href="/uniques/fannar" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a></td>
-      <td><a href="/uniques/josephine" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a> </td>
+  <tr><td></td><td>
+      <a href="/academy/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a> 
+      <a href="/academy/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a> 
+      <a href="/academy/achallengedetails?id=928" title="Edmund, the Human Gladiator (HuGl)"><img src="/img/Edmund.png"></a></td></tr>
+<h4><b>Ranged Weapons</b></h4>
+    <tr><td></td><td>
+      <a href="/academy/achallengedetails?id=929" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a>
+      <a href="/academy/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/Sonja.png"></a>
+      <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/Vashnia.png"></a></td></tr>
+<h4><b>Hybrid: Melee with some Magic</b></h4>
+    <tr><td></td><td>
+      <a href="/academy/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a> 
+      <a href="/academy/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/Ilsuiw_water.png"></a> 
+      <a href="/academy/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></td></tr>
+<h4><b>Mostly Magic</b></h4>
+  <p> <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a> 
+      <a href="/academy/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a> 
+      <a href="/academy/achallengedetails?id=937" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a></td></tr>
   </tr>
 </table>
 Once you have 4 wins with one in each of the 4 playstyles, you'll likely feel ready to <b>compete</b> at an intermediate level in the weekly 
 <a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.
 
-Happy Crawling! :-)
+Happy Crawling! 
+RoGGa ;-p
