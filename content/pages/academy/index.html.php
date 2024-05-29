a3cfed4ae48cd10b8453d5f1dca0bcf2269f24f3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-28 20:56:41 -0400

    Update /academy/index.html.php

--- content/pages/academy/index.html.php
+++ content/pages/academy/index.html.php
@@ -3,22 +3,31 @@
 With the Crawl Cosplay Academy (CCA), you'll play easier combos of different playstyles as you learn to survive longer. And 
 hopefully win many games sooner than later, while playing characters based on 
 <a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
-<h4>Ready?</h4>
+<h3>Ready?</h3>
 Want to know more before jumping in? Read the <a href="/about">About</a> section.
-<h4>Set...</h4>
+<h3>Set...</h3>
 If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed below since they are considered to be the
 <b><i>easier</i></b> combos of the easiest category.
 <br>
-<p><b>Melee</b></p>
-<p><a href="/academy/challengedetails?id=251" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></p>
-<p><a href="/academy/challengedetails?id=291" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></p>
-<p><a href="/academy/challengedetails?id=171" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></p>
-<p><a href="/ranged" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a>
-<p><a href="/ranged" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a><a href="/ranged" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a>&nbsp
-<p><a href="/hybrid" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a><a href="/hybrid" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a><a href="/hybrid" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a>&nbsp
-<p><a href="/magic" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a><a href="/magic" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a><a href="/magic" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a>
-<h4>GO!</h4>
-Once you have completed one of your better games, <a href="/submit">Submit your Morgue</a> to gain access to your porfolio</a>. 
+<h3>GO!</h3>
+<h4><b>Melee</b></h4>
+  <p><a href="/academy/challengedetails?id=251" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></p>
+  <p><a href="/academy/challengedetails?id=291" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></p>
+  <p><a href="/academy/challengedetails?id=171" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></p>
+<h4><b>Ranged</b></h4>
+  <p><a href="/academy/challengedetails?id=231" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a></p>
+  <p><a href="/academy/challengedetails?id=081" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a></p>
+  <p><a href="/academy/challengedetails?id=271" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a></p>
+<h4><b>Hybrid</b></h4>
+  <p><a href="/academy/challengedetails?id=051" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a></p>
+  <p><a href="/academy/challengedetails?id=741" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a></p>
+  <p><a href="/academy/challengedetails?id=331" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></p>
+<h4><b>Magic</b></h4>
+  <p><a href="/academy/challengedetails?id=871" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a></p>
+  <p><a href="/academy/challengedetails?id=331" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a></p>
+  <p><a href="/academy/challengedetails?id=351" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a></p>
+<h3>Submit your Morgue</h3>
+Once you have completed one of your better games, <a href="/submit">Submit your Morgue</a> and a moderator will approve it.</a>. 
 Also, feel free to submit your old morgues that used any of the Unique's specific combos.
 <br /><br />
 <h3>Need more help?</h3>
