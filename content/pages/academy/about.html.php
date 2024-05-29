ef32793e6c47505a4afe5459f27b6d2f9e782288
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-29 07:31:09 -0400

    Update /academy/about.html.php

--- content/pages/academy/about.html.php
+++ content/pages/academy/about.html.php
@@ -13,19 +13,22 @@ After you complete a good game (with either a win or <abbr title="Yet Another Sa
 <br />
 <table>
   <tr><th colspan="2"> Playstyle:</th><th colspan="3">Melee</th><th colspan="3">Ranged</th><th colspan="3">Hybrid</th><th colspan="3">Magic</th></tr>
-  <tr>
-      <th><a href="/uniques/snorg" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></th>
-      <th><a href="/uniques/asterion" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></th>
-      <th><a href="/uniques/saint-roka" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></th>
-      <th><a href="/uniques/nessos" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a></th>
-      <th><a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a></th>
-      <th><a href="/uniques/joseph" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a></th>
-      <th><a href="/uniques/grum" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a></th>
-      <th><a href="/uniques/ilsuiw" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a></th>
-      <th><a href="/uniques/jory" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></th>
-      <th><a href="/uniques/tiamat" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a></th>
-      <th><a href="/uniques/fannar" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a></th>
-      <th><a href="/uniques/josephine" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a> </th></tr>
+  <tr><td></td>
+      <td><a href="/uniques/snorg" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a></td>
+      <td><a href="/uniques/asterion" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a></td>
+      <td><a href="/uniques/saint-roka" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a></td>
+      <td><a href="/uniques/nessos" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a></td>
+      <td><a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a></td>
+      <td><a href="/uniques/joseph" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a></td>
+      <td><a href="/uniques/grum" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a></td>
+      <td><a href="/uniques/ilsuiw" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a></td>
+      <td><a href="/uniques/jory" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a></td>
+      <td><a href="/uniques/tiamat" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a></td>
+      <td><a href="/uniques/fannar" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a></td>
+      <td><a href="/uniques/josephine" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a> </td>
+  </tr>
 </table>
-Once you are certified, you'll likely feel ready to compete at an intermediate to advanced level in the weekly 
+Once you have 4 wins with one in each of the 4 playstyles, you'll likely feel ready to <b>compete</b> at an intermediate level in the weekly 
 <a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.
+
+Happy Crawling! :-)
