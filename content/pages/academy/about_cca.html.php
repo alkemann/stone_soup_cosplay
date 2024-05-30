4836359b4a0445addd94227563ab5cf8cd5a065c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-30 12:05:28 -0400

    Update about_cca.html.php

--- content/pages/academy/about_cca.html.php
+++ content/pages/academy/about_cca.html.php
@@ -1,14 +1,19 @@
 <?php $this->layout = 'academy'; ?>
 <h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Academy (CCA)</h2>
+
+<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">TLDR: Want to improve your gameplay while having fun doing it? 
+With the Crawl Cosplay Academy (CCA), you'll play easier combos of different playstyles as you learn to survive longer. And 
+hopefully win many games sooner than later, while playing characters based on 
+<a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
+
+<h3>The gists</h3>
 The <b>Crawl Cosplay Academy</b> is intended as a prequel to the weekly <a href="/" target="_blank">Crawl Cosplay Challenge</a> (CCC)...and yet, 
 on its own, it is so much more.
 <p>As a prequel, it's for players who don't feel they have the skills necessary to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.</p>
 <p>In general though, the audience is <abbr title="Dungeon Craw: Stone Soup">DCSS</abbr> players who are fairly new to the game or 
 haven't won at least a game with each of the four playstyles.
 
-<h3>Process</h3>
-After you complete a good game (with either a win or <abbr title="Yet Another Sad Death">YASD</abbr>), you <b>Submit your Morgue</b> to track your progress.
-<br />
+<h3>Your Process</h3>
 <p>There are 4 playstyles with 3 uniques each:
   </b>Melee Weapons</b>, <b>Ranged Weapons</b>, <b>Hybrid</b>, and <b>Magic</b>. </p>
 <h4><b>Melee Brutes</b></h4>
@@ -27,10 +32,12 @@ After you complete a good game (with either a win or <abbr title="Yet Another Sa
   <p> <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat_green.png"></a> 
       <a href="/academy/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a> 
       <a href="/academy/achallengedetails?id=937" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a>
-<br><br>
+<br>
+After you complete a good game (with either a win or <abbr title="Yet Another Sad Death">YASD</abbr>), you <b>Submit your Morgue</b> to track your progress.
+<br />
 Once you have 4 wins with one in each of the 4 playstyles, you'll likely feel ready to <b>compete</b> at an intermediate level in the weekly 
 <a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.
-<br><br>
+<br>
 Happy Crawling!<br> 
 RoGGa ;-p
 <br>
