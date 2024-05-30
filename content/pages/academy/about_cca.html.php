f36bd8300bf6221a085c55b271501d6a7f7e87b3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-30 07:09:55 -0400

    Update about_cca.html.php

--- content/pages/academy/about_cca.html.php
+++ content/pages/academy/about_cca.html.php
@@ -11,13 +11,12 @@ After you complete a good game (with either a win or <abbr title="Yet Another Sa
 <br />
 <p>There are 4 playstyles with 3 uniques each:
   </b>Melee Weapons</b>, <b>Ranged Weapons</b>, <b>Hybrid</b>, and <b>Magic</b>. </p>
-
 <h4><b>Melee Brutes</b></h4>
       <a href="/academy/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a> 
       <a href="/academy/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a> 
       <a href="/academy/achallengedetails?id=928" title="Edmund, the Human Gladiator (HuGl)"><img src="/img/Edmund.png"></a>
 <h4><b>Ranged Weapons</b></h4>
-      <a href="/academy/achallengedetails?id=929" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a>
+      <a href="/academy/achallengedetails?id=929" title="Joseph, the Human Hunter (HuHu)"><img src="/img/Joseph.png"></a>
       <a href="/academy/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/Sonja.png"></a>
       <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/Vashnia.png"></a></td></tr>
 <h4><b>Hybrid: Melee mostly with some Magic</b></h4>
@@ -34,3 +33,4 @@ Once you have 4 wins with one in each of the 4 playstyles, you'll likely feel re
 <br><br>
 Happy Crawling!<br> 
 RoGGa ;-p
+<br>
