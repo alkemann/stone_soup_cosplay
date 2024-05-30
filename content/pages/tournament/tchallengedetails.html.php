37101ecf1fd339feefce076796307e0ec19e2681
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-29 20:08:10 -0400

    Update tchallengedetails.html.php
    added Reddit link

--- content/pages/tournament/tchallengedetails.html.php
+++ content/pages/tournament/tchallengedetails.html.php
@@ -39,6 +39,7 @@ $this->setData("meta", ['filename' => $cha->icon]);
 	<?php endif;?>
 	<?php if ($cha->wiki): ?> |
 	<a href="<?=$e($cha->wiki)?>">Wiki page</a><?php endif; ?>
+	| <a href="<?=$e($cha->reddit)?>">See Reddit post</a>
 </p>
 
 <?php if ($cha->icon) : ?><img src="<?=$e($cha->icon)?>" class="detail" /><?php endif; ?>
