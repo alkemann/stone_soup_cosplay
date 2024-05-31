135cd77860e1889f879bb3a7d51fcc31c20ab6cd
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-31 12:08:45 -0400

    Create /admin/head.html.php

---
+++ content/layouts/admin/head.html.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?=$page_title ?? 'Crawl Cosplay Challenge'?></title>
+    <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
+    <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
+    <link rel="stylesheet" href="/css/admin.css?v=<?=time()?>">
+    <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
+
+<?php
+
+    if (isset($meta) && is_array($meta) && isset($meta['filename'])) {
+        $meta += [
+            'width' => 256,
+            'height' => 256,
+            'alt' => "DCSS Cosplay Challenge",
+            'type' => "image/png",
+        ];
+        if (strpos($meta['filename'], 'https') === 0) {
+            $meta['secure_filename'] = $meta['filename'];
+            $meta['filename'] = str_replace("https", "http", $meta['filename']);
+        } else {
+            $meta['secure_filename'] = str_replace("http", "https", $meta['filename']);
+        }
+        echo <<<META
+            <meta property="og:image" content="{$meta['filename']}" />
+            <meta property="og:image:secure_url" content="{$meta['secure_filename']}" />
+            <meta property="og:image:type" content="{$meta['type']}" />
+            <meta property="og:image:width" content="{$meta['width']}" />
+            <meta property="og:image:height" content="{$meta['height']}" />
+            <meta property="og:image:alt" content="{$meta['alt']}" />
+        META;
+    }
+
+?>
+</head>
+<body class="page_back">
+<div class="page_floor">
