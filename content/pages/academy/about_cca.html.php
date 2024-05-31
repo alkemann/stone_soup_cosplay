2b3c1ec174607576ab5c16a1f320dbbc23c29667
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-31 12:46:55 -0400

    Update about_cca.html.php

--- content/pages/academy/about_cca.html.php
+++ content/pages/academy/about_cca.html.php
@@ -1,7 +1,8 @@
 <?php $this->layout = 'academy'; ?>
 <h2 style="color:rgb(69, 136, 5);">About Crawl Cosplay Academy (CCA)</h2>
 
-<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">TLDR: Want to improve your gameplay while having fun doing it? 
+<h3>TLDR</h3>
+<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">Want to improve your gameplay while having fun doing it? 
 With the Crawl Cosplay Academy (CCA), you'll play easier combos of different playstyles as you learn to survive longer. And 
 hopefully win many games sooner than later, while playing characters based on 
 <a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
@@ -18,30 +19,35 @@ haven't won at least a game with each of the four playstyles.
 <h3>Your Process</h3>
 <p>There are 4 playstyles with 3 uniques each:
   <b>Melee Weapons</b>, <b>Ranged Weapons</b>, <b>Hybrid</b>, and <b>Magic</b>. </p>
-  Over over the Uniques' graphic to see their descriptions, and click on one of them to open the CCA Challenge's details page.
-<h4><b>Melee Brutes</b></h4>
+  Hover over a Unique's graphic to see their descriptions, and then click on one of them to open the Unique's challenge's details page.
+<table><tr>
+    <td><p><b>Melee Brutes</b></p>
       <a href="/academy/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a> 
       <a href="/academy/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a> 
       <a href="/academy/achallengedetails?id=928" title="Edmund, the Human Gladiator (HuGl)"><img src="/img/Edmund.png"></a>
-<h4><b>Ranged Weapons</b></h4>
+    </td>
+    <td><p><b>Ranged Weapons</b></p>
       <a href="/academy/achallengedetails?id=929" title="Joseph, the Human Hunter (HuHu)"><img src="/img/Joseph.png"></a>
       <a href="/academy/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/Sonja.png"></a>
-      <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/Vashnia.png"></a></td></tr>
-<h4><b>Hybrid: Melee mostly with some Magic</b></h4>
+      <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/Vashnia.png"></a>
+    </td>
+    <td><p><b><abbr title="Melee mostly with some Magic">Hybrid</b></p>
       <a href="/academy/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a> 
       <a href="/academy/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/Ilsuiw_water.png"></a> 
       <a href="/academy/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a>
-<h4><b>Mostly Magic</b></h4>
-  <p> <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat_green.png"></a> 
+    </td>
+    <td><p><b>Mostly Magic</b></p>
+      <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat_green.png"></a> 
       <a href="/academy/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a> 
       <a href="/academy/achallengedetails?id=937" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a>
-<br>
+    </td>
+</table>
 <p>Each CCA challenge has 3 conducts and 2 bonuses. The 3 conducts are specific to getting you to the Lair and/or Orcish Mines entrance(s).</p>
    NOTE: Do not go passed D:12 before clearing the Lair and the Orcish Mines, as you will likely experiece <abbr title="Yet Another Sad Death">YASD</abbr>!
-<p>Conducts 1 &2 will usually be related to decisions you need to take in the early game, and the 3rd conduct will usually be a small challenge to add some fun.</p>
-<p>The 1st bonus can usually be accomplished after clearing the Dungeon (D), Lair (L) & the Orcish Mines (O), while the 2nd bonus will be a bit more challenging and will usually involve something like: "...and get your first rune!" as a caveate.</p>
+<p>Conducts 1 & 2 will usually be related to decisions you need to take in the early game, and the 3rd conduct will usually be a small challenge to add some fun.</p>
+<p>The 1st bonus can usually be accomplished by the time you've cleared the Dungeon (D), Lair (L) & the Orcish Mines (O), while the 2nd bonus will be a bit more challenging and will usually involve something like: "...and get your first rune!" as a caveate.</p>
 <br>
-After you complete a good game (with either a win or <abbr title="Yet Another Sad Death">YASD</abbr>), you <b>Submit your Morgue</b> to track your progress.
+After you complete a good game (with either a <abbr title="aka Yet Another Victory Post (YAVP)">win</abbr>) or <abbr title="Yet Another Sad Death">YASD</abbr>), just <b>Submit your Morgue</b> on the Unique's challenge details webpage. To track your progress, click on your name on that last page mentionned.
 <br />
 Once you have 4 wins with one in each of the 4 playstyles, you'll likely feel ready to <b>compete</b> at an intermediate level in the weekly 
 <a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.
