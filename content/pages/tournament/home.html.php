0d3508d4654d42e6e5c659148361e06fd3aee367
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-31 19:39:14 -0400

    Update /tournament/home.html.php

--- content/pages/tournament/home.html.php
+++ content/pages/tournament/home.html.php
@@ -22,7 +22,7 @@ if ($active) :
 <p><a href="/tournament/tchallengedetails?id=<?=$e($active->id)?>">Challenge details</a>  
  | <a href="/submit?id=<?=$e($active->id)?>">Submit a run</a>
  | <a href="https://discord.gg/ZQ4kk6n">Discuss it in CCC's Discord</a> 
- |<a href="<?=$e($active->reddit)?>">Reddit post</a>
+ | <a href="<?=$e($active->reddit)?>">Reddit post</a>
  | Next challenge starts on Friday at 0:00 UTC.</p>
 <table class="table_for_layout">
 	<tr><th>Species</th><th>Background<th>Gods</th></tr>
