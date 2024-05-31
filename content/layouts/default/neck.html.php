a109e37d235f03bc63fef5bc3897d9010170c196
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-05-31 19:10:02 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -27,7 +27,7 @@
     <div class="page_content">
         <div class="heading">
             <h1><a href="/"><img src="/img/ccc_stone_soup_icon-512x512.png" width="48" height="48" />Crawl Cosplay Challenge<img src="/img/ccc_stone_soup_icon-512x512.png" width="48" height="48" /></a></h1>
-            <div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="12" height="12" ></a></div>
+            <div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></div>
         </div>
         <br />
         <img src="/img/HR-right.png"><br />
