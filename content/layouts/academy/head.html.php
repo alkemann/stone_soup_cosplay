df47af17e55711e28e8b5cbac95604f5b48a080f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-01 00:52:06 -0400

    Update /academy/head.html.php

--- content/layouts/academy/head.html.php
+++ content/layouts/academy/head.html.php
@@ -5,7 +5,7 @@
     <title><?=$page_title ?? 'Crawl Cosplay Academy (CCA)'?></title>
     <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
-    <link rel="stylesheet" href="/css/academy.css?v=<?=time()?>">
+    <link rel="stylesheet" href="/css/cca.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
 
 <?php
