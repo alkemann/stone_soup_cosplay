17f1e389763304d287880c3c28aba162523ff788
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 14:15:23 -0400

    Update academy.html.php
    add scoring table

--- content/pages/academy.html.php
+++ content/pages/academy.html.php
@@ -1,4 +1,16 @@
-<?php $this->layout = 'cca'; ?>
+<?php $this->layout = 'cca';
+use app\models\{Challenge, Submission, Player};
+
+$this->layout = 'tournament';
+
+$active = Challenge::tournamentActive();
+if ($active) :
+ 	$set = 0;
+	$scores = Player::scoreboardForSet($set);
+	$challenges_in_set = Challenge::findAsArray(['setnr' => $set, 'draft' => 0], ['order' => '`week` ASC']);
+	$weeks = sizeof($challenges_in_set);
+	$this->setData("meta", ['filename' => $active->icon]);
+?>
 <h2 style="color:rgb(69, 136, 5);">Welcome green Crawler!</h2>
 <h3>Ready?</h3>
 Want to know more before jumping in? Read the <a href="/academy/about_cca">About CCA</a> webpage.
@@ -30,3 +42,68 @@ Your best 2 runs for each challenge will be kept in the website's database...so 
 <h3>Need more help?...or have some questions</h3>
 <p>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Crawl Cosplay Discord server</a> in the <b>#academy_cca</b> text channel.
 <br>
+<h2>Tournament Scoreboard for Trunk v0.<?=$e($active->setnr)?></h2>
+<table class="set-list">
+<?php
+	$made_seperator = false;
+	foreach ($challenges_in_set as $cha) :
+		if ($cha->bonus && !$made_seperator) {
+			echo '<tr><td colspan="3">&nbsp;</td></tr>';
+			$made_seperator = true; // only make one seperator if multiple bonuses
+		}
+?>
+	<tr>
+		<td>Week <?=$e($cha->week)?>.</td>
+		<td><?php if ($cha->icon):?><img src="<?=$e($cha->icon)?>" style="height: 1em" /><?php endif; ?> <b><a href="/tournament/tchallengedetails?id=<?=$e($cha->id)?>"><?=$e($cha->name)?></a></b></td>
+		<td><span style="font-size: smaller"><?=$e($cha->shortform())?></span></td>
+	</tr>
+<?php endforeach; ?>
+</table>
+
+<table class="bordered">
+	<tr>
+		<th>Player</th>
+		<th>Total <span class="star">&#9733;</span></th>
+		<?php
+		$made_seperator = false;
+		foreach ($challenges_in_set as $c) {
+
+			if ($c->bonus && !$made_seperator) {
+				echo '<th rowspan="' . (sizeof($scores) + 1) . '">&nbsp;</th>';
+				$made_seperator = true; // only make one seperator if multiple bonuses
+			}
+			echo '<th>' . $e($c->week) . '. ';
+			if ($cha->icon) echo '<a href="/challenges/details?id='.$c->id.'"><img src="'.$e($c->icon).'" style="height: 1.5em" /></a>';
+			echo "</th>";
+		}
+		?>
+	</tr>
+	<?php
+	foreach ($scores as $i => $row) : ?>
+		<tr class="<?=$i%2==0?'odd':'even'?>">
+			<td><a href="/player?id=<?=$e($row['pid'])?>"><?=$e($row['player'])?></a></td>
+			<td><?=$e($row['total'])?> <?=$e($row['stars'])?><span class="star">&#9733;</span></td>
+			<?php
+
+			foreach ($challenges_in_set as $c) {
+				$week = $row['week'][$c->id];
+				if ($week == null) {
+					echo "<td>&nbsp;</td>";
+					continue;
+				}
+				$out = '<td>';
+				if (!empty($week['morgue'])) $out .= '<a href="'. $e($week['morgue']) .'" target="_blank">';
+				$out .= $week['score'];
+				for ($i=0; $i < (int) $week['stars'] ; $i++) {
+					$out .= '<span class="star">&#9733;</span>';
+				}
+				if (!empty($week['morgue'])) $out .= '</a>';
+				echo $out . "</td>";
+			}
+			for ($i=0; $i < $weeks - sizeof($row['week']); $i++) {
+				echo "<td>&nbsp;</td>";
+			}
+			?>
+		</tr>
+	<?php endforeach; ?>
+</table>
