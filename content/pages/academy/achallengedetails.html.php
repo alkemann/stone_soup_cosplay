a47306ba6cf1cb42407ff66c9f1ae95d279df69c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 15:27:52 -0400

    Update achallengedetails.html.php
    reactivated Special Rule and name it Strategy for CCA challenges

--- content/pages/academy/achallengedetails.html.php
+++ content/pages/academy/achallengedetails.html.php
@@ -42,12 +42,10 @@ $this->setData("meta", ['filename' => $cha->icon]);
 <p class="info">The Species, Background, and God choices are all mandatory. You must be worshipping one of the gods listed above before entering Lair, Orcish Mines, the Vaults or Depths, unless this isn't possible in which case you must worship them as soon as you can. Don't use faded altars (except in challenges where you can choose any god), and don't do anything to lose your religion unless otherwise specified.</p>
 <p class="info">NOTE: Except for Beogh, Ignis, Jiyva and Lugonu, an altar for the other gods will ALWAYS show up by D:10.</p>
 
-<!-- 
-	<?php if ($cha->special_rule) : ?>
-		<h3>Special Rule or Notes</h3>
-		<div class="special_rule"><p><?=$em($cha->special_rule)?></p></div>
-	<?php endif; ?>
--->
+<?php if ($cha->special_rule) : ?>
+	<h3>Strategy</h3>
+	<div class="special_rule"><p><?=$em($cha->special_rule)?></p></div>
+<?php endif; ?>
 	
 <div class="score-sidebar">
 <h3>Submissions</h3>
