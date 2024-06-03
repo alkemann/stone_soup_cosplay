3fbf4836013314cff6c19aeecf698580a864cf07
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 02:09:47 -0400

    update /admin/neck

--- content/layouts/admin/neck.html.php
+++ content/layouts/admin/neck.html.php
@@ -1,32 +1,31 @@
 <div class="page information">
     <div class="toplinks">
-          <a href="/">Crawl Cosplay Home</a>
-	| <a href="/about_ccc">About CCC</a>
-	| <a href="/ccc_home">Weekly active CCC</a>
-        | <a href="/history">All CCC Challenges</a>
+    <a href="/">Crawl Cosplay Home</a>
+	   | <a href="/history">All CCC Challenges</a>
+	   | <a href="/about_ccc">About CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+	   | <a href="/ccc_home">Weekly active CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+    	<br>
+	   | <a href="/tournament/about">About CCTT</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+	   | <a href="/tournament/home">Tournament (CCTT)</a> NEW!
+    	<br>
+	   | <a href="/academy/about_cca">About CCA</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+	   | <a href="/academy">Academy (CCA)</a>&nbsp&nbsp&nbsp&nbsp&nbspNEW!
 	<br>
-          <a href="/tournament/about">About CCTT</a>
-	| <a href="/tournament/home">Tournament (CCTT)</a>
+	   | <a href="/recent">All Submissions</a>
+	   - to be moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>&nbsp&nbsp&nbsp&nbsp
 	<br>
-          <a href="/academy/about_cca">About CCA</a>
-	| <a href="/academy">Academy (CCA)</a>
-	<br>
-          <a href="/recent">All Submissions</a>
-        (to be moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>)
-	<br>
-    <?php if ($this->request->session('admin')) : ?>
-          Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
-        | Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
-      	| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
-        | <a href="/logout">Logout</a>
-
+	<?php if ($this->request->session('admin')) : ?>
+	          Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
+        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
+	    	| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
+        	| <a href="/logout">Logout</a>
 	<?php else : ?>
-	     <a href="/backoffice">Admin</a>
+	          <div class="fineprint"><a href="/backoffice">Admin</a></div>
 	<?php endif; ?>
     </div>
     <div class="page_content">
         <div class="heading">
-            <h1><a href="/"><img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" />Crawl Cosplay /admin/<img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /></a></h1>
+            <h1><img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" />Crawl Cosplay /admin/<img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
         </div>
         <br />
         <img src="/img/HR-right.png"><br />
