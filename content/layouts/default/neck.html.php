b4eb4b7d1ff41c33cec289dbd4c73f7437f957e7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 12:58:57 -0400

    Update /default/neck.html.php
    added dropdown menus

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -1,35 +1,41 @@
 <div class="page information">
-    <div class="toplinks">
-          <a href="/">Crawl Cosplay Home</a>
-	| <a href="/about_ccc">About CCC</a>
-	| <a href="/ccc_home">Weekly active CCC</a>
-        | <a href="/history">All CCC Challenges</a>
-	<br>
-          <a href="/tournament/about">About CCTT</a>
-	| <a href="/tournament/home">Tournament (CCTT)</a>
-	<br>
-          <a href="/academy/about_cca">About CCA</a>
-	| <a href="/academy">Academy (CCA)</a>
-	<br>
-          <a href="/recent">All CC Submissions</a>
-        (to be moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>)
-	<br>
-    <?php if ($this->request->session('admin')) : ?>
-          Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
-        | Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
-    	| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
-        | <a href="/logout">Logout</a>
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">About</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/academy/about_cca">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/about_ccc">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/tournament/about">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+		</div>
+	</div>
 
-	<?php else : ?>
-	     <a href="/backoffice">Admin</a>
-	<?php endif; ?>
-    </div>
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Crawl Cosplay</button>
+		<div class="dropdown-content">
+			<a href="/">Crawl Cosplay Home</a>
+			<a href="/academy">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/ccc_home">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
+			<a href="/tournament/home">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+		</div>
+	</div>
+
+    <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
             <h1><center><img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" />Crawl Cosplay<img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /></center></h1>
             <div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
         </div>
-        <br />
+	<div class="fineprint" align="right">
+   		To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
+		<?php if ($this->request->session('admin')) : ?>
+        		| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
+	        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
+    			| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
+	        	| <a href="/logout">Logout</a>
+		<?php else : ?>
+          		| <a href="/backoffice">Admin</a>
+		<?php endif; ?>
+	</div>
         <img src="/img/HR-right.png"><br />
         <div class="content" onclick="window.location = '/dismiss';">
         <?php if ($msg = $this->request->session()->get('message')) : ?>
