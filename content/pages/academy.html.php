eaa88502333a51b4356ce666467ba687982c5906
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 14:20:55 -0400

    Update academy.html.php
    sumbit typo

--- content/pages/academy.html.php
+++ content/pages/academy.html.php
@@ -37,7 +37,7 @@ Just click on one of the Uniques for the challenge's details listing the suggest
       <a href="/academy/achallengedetails?id=937" title="Jorgrun, the Mountain Dwarf Earth Elementalist (MDEE)"><img src="/img/Jorgrun.png" width="48" height="48"></a></p>
 <h3>GO!</h3>
 Play a game of your favourite DCSS Uniques and once you have completed one of your better games, <b>Submit your Morgue</b> on the challenge's detail page and a moderator will approve it.
-Your best 2 runs for each challenge will be kept in the website's database...so sumbit as often as you like.
+Your best 2 runs for each challenge will be kept in the website's database...so submit as often as you like.
 <br>
 <h3>Need more help?...or have some questions</h3>
 <p>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Crawl Cosplay Discord server</a> in the <b>#academy_cca</b> text channel.
