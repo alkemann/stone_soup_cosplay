9ba7e63c60df4b2600b22e7fb1a7bc9b3d609cf8
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-03 12:20:55 -0400

    Update /academy/neck.html.php

--- content/layouts/academy/neck.html.php
+++ content/layouts/academy/neck.html.php
@@ -25,17 +25,17 @@
             <h1><img src="/img/cca_stone_soup_icon-512x512.png" width="48" height="48" />Crawl Cosplay Academy<img src="/img/cca_stone_soup_icon-512x512.png" width="48" height="48" /></h1>
             <div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/WdbyURBcYp" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
         </div>
-		<div class="fineprint" align="right">
-	   		To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
-			<?php if ($this->request->session('admin')) : ?>
-	        		| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
-		        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
-	    			| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
-		        	| <a href="/logout">Logout</a>
-			<?php else : ?>
-	          		<a href="/backoffice">Admin</a>
-			<?php endif; ?>
-		</div>
+	<div class="fineprint" align="right">
+   		To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
+		<?php if ($this->request->session('admin')) : ?>
+        		| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
+	        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
+    			| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
+	        	| <a href="/logout">Logout</a>
+		<?php else : ?>
+          		<a href="/backoffice">Admin</a>
+		<?php endif; ?>
+	</div>
         <br><img src="/img/HR-right.png"><br>
         <div class="content" onclick="window.location = '/dismiss';">
         <?php if ($msg = $this->request->session()->get('message')) : ?>
