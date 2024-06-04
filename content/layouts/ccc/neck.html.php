654baef8b3670c9813e30d09af7da2e062c415b4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-04 02:05:17 -0400

    Update neck.html.php

--- content/layouts/ccc/neck.html.php
+++ content/layouts/ccc/neck.html.php
@@ -1,19 +1,25 @@
 <div class="page information">
-    <div class="toplinks">
-	     <a href="/">Crawl Cosplay Home</a>
-	   | <a href="/history">All CCC Challenges</a>
-	   | <a href="/about_ccc">About CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-	   | <a href="/ccc_home">Weekly active CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp
-    	<br>
-	   | <a href="/tournament/about">About CCTT</a>&nbsp&nbsp&nbsp&nbsp&nbsp
-	   | <a href="/tournament/home">Tournament (CCTT)</a> NEW!
-    	<br>
-	   | <a href="/academy/about_cca">About CCA</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-	   | <a href="/academy">Academy (CCA)</a>&nbsp&nbsp&nbsp&nbsp&nbspNEW!
-	<br>
-	   | <a href="/recent">All Submissions</a>
-	   - to be moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>&nbsp&nbsp&nbsp&nbsp
-	<br>
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">About...</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/academy/about_cca">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/about_ccc">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/tournament/about">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+		</div>
+	</div>
+
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Crawl Cosplay Menu</button>
+		<div class="dropdown-content">
+			<a href="/">Crawl Cosplay Home</a>
+			<a href="/academy">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/ccc_home">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
+			<a href="/tournament/home">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+		</div>
+	</div>
+
+    	<div class="toplinks"><br></div>
 	<?php if ($this->request->session('admin')) : ?>
 	          Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
         	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
