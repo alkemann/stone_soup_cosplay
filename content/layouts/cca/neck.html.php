d9cb46e292917548523cea1824a1085d10dd0c13
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-04 02:18:32 -0400

    Update /cca/neck.html.php
    1 dropdown menu

--- content/layouts/cca/neck.html.php
+++ content/layouts/cca/neck.html.php
@@ -1,21 +1,15 @@
 <div class="page information">
-	<div class="dropdown" style="float:left;">
-		<button class="dropbtn">About...</button>
-		<div class="dropdown-content" style="left:0;">
-			<a href="/academy/about_cca">Crawl Cosplay Academy (CCA) - NEW!</a>
-			<a href="/about_ccc">Crawl Cosplay Challenge (CCC)</a>
-			<a href="/tournament/about">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
-		</div>
-	</div>
-
 	<div class="dropdown" style="float:right;">
-		<button class="dropbtn">Crawl Cosplay Menu</button>
+		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content">
 			<a href="/">Crawl Cosplay Home</a>
 			<a href="/academy">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/academy/about_cca"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCA</a>
 			<a href="/ccc_home">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/about_ccc"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCC</a>
 			<a href="/history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
 			<a href="/tournament/home">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+			<a href="/tournament/about"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCTT</a>
 		</div>
 	</div>
 
