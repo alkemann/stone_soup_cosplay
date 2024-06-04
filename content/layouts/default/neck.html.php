9cb8107c3b66e9f7d2611d083508a08864b0f4a7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-04 07:32:15 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -1,44 +1,38 @@
 <div class="page information">
 	<div class="dropdown" style="float:left;">
-		<button class="dropbtn">About...</button>
+		<button class="dropbtn">Menu</button>
 		<div class="dropdown-content" style="left:0;">
-			<a href="/academy/about_cca">Crawl Cosplay Academy (CCA) - NEW!</a>
-			<a href="/about_ccc">Crawl Cosplay Challenge (CCC)</a>
-			<a href="/tournament/about">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
-		</div>
-	</div>
-
-	<div class="dropdown" style="float:right;">
-		<button class="dropbtn">Crawl Cosplay menu</button>
-		<div class="dropdown-content">
 			<a href="/">Crawl Cosplay Home</a>
 			<a href="/academy">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/academy/about_cca"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCA</a>
 			<a href="/ccc_home">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/about_ccc"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCC</a>
 			<a href="/history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
 			<a href="/tournament/home">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+			<a href="/tournament/about"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCTT</a>
 		</div>
 	</div>
 
-    <div class="toplinks"><br></div>
-    <div class="page_content">
-        <div class="heading">
-            <h1><center><img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /> Crawl Cosplay <img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /></center></h1>
-            <div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
-        </div>
-	<div class="fineprint" align="right">
-   		To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
-		<?php if ($this->request->session('admin')) : ?>
-        		| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
-	        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
-    			| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
-	        	| <a href="/logout">Logout</a>
-		<?php else : ?>
-          		| <a href="/backoffice">Admin</a>
-		<?php endif; ?>
-	</div>
-        <img src="/img/HR-right.png"><br />
-        <div class="content" onclick="window.location = '/dismiss';">
-        <?php if ($msg = $this->request->session()->get('message')) : ?>
-            <div class="message"><?=$msg?> <br /><br /><a href="/dismiss">--more--</a></div>
-        <?php endif; ?>
-        </div>
+    	<div class="toplinks"><br></div>
+    	<div class="page_content">
+        	<div class="heading">
+            		<h1><center><img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /> Crawl Cosplay <img src="/img/cc_stone_soup_icon-512x512.png" width="48" height="48" /></center></h1>
+            		<div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
+        	</div>
+		<div class="fineprint" align="right">
+   			To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
+			<?php if ($this->request->session('admin')) : ?>
+        			| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
+	        		| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
+    				| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
+	        		| <a href="/logout">Logout</a>
+			<?php else : ?>
+          			| <a href="/backoffice">Admin</a>
+			<?php endif; ?>
+		</div>
+        	<img src="/img/HR-right.png"><br />
+        	<div class="content" onclick="window.location = '/dismiss';">
+        		<?php if ($msg = $this->request->session()->get('message')) : ?>
+            			<div class="message"><?=$msg?> <br /><br /><a href="/dismiss">--more--</a></div>
+        		<?php endif; ?>
+        	</div>
