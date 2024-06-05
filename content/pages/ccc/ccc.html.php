ac24e13358ed19406c895f1a5ed26bc3a9bd0920
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-05 07:18:41 -0400

    Create ccc.html.php

---
+++ content/pages/ccc/ccc.html.php
@@ -0,0 +1,108 @@
+<?php $this->layout = 'ccc'; ?>
+<?php
+use app\models\{Challenge, Submission, Player};
+$active = Challenge::active();
+if ($active) :
+	$set = $active->setnr;
+	$scores = Player::scoreboardForSet($set);
+	$challenges_in_set = Challenge::findAsArray(['setnr' => $set, 'draft' => 0], ['order' => '`week` ASC']);
+	$weeks = sizeof($challenges_in_set);
+	$this->setData("meta", ['filename' => $active->icon]);
+?>
+<h2>
+	Current challenge Set <?=$e($active->setnr)?> Week <?=$e($active->week)?>: <a href="/challenges/details?id=<?=$e($active->id)?>"><?=$e($active->name)?></a>
+	<?php if ($active->icon): ?><img src="<?=$e($active->icon)?>" class="head-icon" height="30px" /> <?php endif; ?>
+</h2>
+<p style="font-style: italic; color: #777;"><?=$e($active->description)?></p>
+<p><a href="/challenges/details?id=<?=$e($active->id)?>">Challenge details</a>  | <a href="/submit">Submit a run</a> | <a href="https://discord.gg/ZQ4kk6n">Discuss it in CCC's Discord</a> | Next challenge starts on Monday.</p>
+<table class="table_for_layout">
+	<tr><th>Species</th><th>Background<th>Gods</th></tr>
+	<tr><td><?=$e($active->species)?></td><td><?=$e($active->background)?><td><?=$e($active->gods)?></td></tr>
+</table>
+<?php if ($active->special_rule) : ?>
+<div class="special_rule"><p><?=$em($active->special_rule)?></p></div>
+<?php endif; ?>
+
+<img src="/img/HR-right.png"><br />
+
+<!-- disable video news bar for now
+<!-- ?php echo $this->part('latest_twitch'); ?> -->
+
+<h2>Scoreboard for Set <?=$e($active->setnr)?></h2>
+<table class="set-list">
+<?php
+	$made_seperator = false;
+	foreach ($challenges_in_set as $cha) :
+		if ($cha->bonus && !$made_seperator) {
+			echo '<tr><td colspan="3">&nbsp;</td></tr>';
+			$made_seperator = true; // only make one seperator if multiple bonuses
+		}
+?>
+	<tr>
+		<td>Week <?=$e($cha->week)?>.</td>
+		<td><?php if ($cha->icon):?><img src="<?=$e($cha->icon)?>" style="height: 1em" /><?php endif; ?> <b><a href="/challenges/details?id=<?=$e($cha->id)?>"><?=$e($cha->name)?></a></b></td>
+		<td><span style="font-size: smaller"><?=$e($cha->shortform())?></span></td>
+	</tr>
+<?php endforeach; ?>
+</table>
+
+<table class="bordered">
+	<tr>
+		<th>Player</th>
+		<th>Total <span class="star">&#9733;</span></th>
+		<?php
+		$made_seperator = false;
+		foreach ($challenges_in_set as $c) {
+
+			if ($c->bonus && !$made_seperator) {
+				echo '<th rowspan="' . (sizeof($scores) + 1) . '">&nbsp;</th>';
+				$made_seperator = true; // only make one seperator if multiple bonuses
+			}
+			echo '<th>' . $e($c->week) . '. ';
+			if ($cha->icon) echo '<a href="/challenges/details?id='.$c->id.'"><img src="'.$e($c->icon).'" style="height: 1.5em" /></a>';
+			echo "</th>";
+		}
+		?>
+	</tr>
+	<?php
+	foreach ($scores as $i => $row) : ?>
+		<tr class="<?=$i%2==0?'odd':'even'?>">
+			<td><a href="/player?id=<?=$e($row['pid'])?>"><?=$e($row['player'])?></a></td>
+			<td><?=$e($row['total'])?> <?=$e($row['stars'])?><span class="star">&#9733;</span></td>
+			<?php
+
+			foreach ($challenges_in_set as $c) {
+				$week = $row['week'][$c->id];
+				if ($week == null) {
+					echo "<td>&nbsp;</td>";
+					continue;
+				}
+				$out = '<td>';
+				if (!empty($week['morgue'])) $out .= '<a href="'. $e($week['morgue']) .'" target="_blank">';
+				$out .= $week['score'];
+				for ($i=0; $i < (int) $week['stars'] ; $i++) {
+					$out .= '<span class="star">&#9733;</span>';
+				}
+				if (!empty($week['morgue'])) $out .= '</a>';
+				echo $out . "</td>";
+			}
+			for ($i=0; $i < $weeks - sizeof($row['week']); $i++) {
+				echo "<td>&nbsp;</td>";
+			}
+			?>
+		</tr>
+	<?php endforeach; ?>
+</table>
+
+
+<?php else : // no active challenge
+	echo "<h2>There is currently no active challenge for the weekly Crawl Cosplay Challenge (CCC) series.</h2>";
+	echo '<p>For your info, you can still submit a run/morgue for <a href="https://cosplay.kelbi.org/history">any CCC challenge</a>.</p>';
+
+	$tournament_challenge = Challenge::tournamentActive();
+	if ($tournament_challenge) {
+		echo '<p>There is an active tournament right now you can participate in! <a href="/tournament/home">Go there now!</a>';
+  	}
+	echo "<br>";
+endif;
+?>
