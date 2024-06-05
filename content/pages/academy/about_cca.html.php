89e3b6b4196e9d23900e19ddb0564d4f6b837625
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-05 08:18:57 -0400

    Update about_cca.html.php
    added Q&A section

--- content/pages/academy/about_cca.html.php
+++ content/pages/academy/about_cca.html.php
@@ -50,14 +50,16 @@ haven't won at least a couple of games.</p>
 After you complete a good game (with either a <abbr title="aka Yet Another Victory Post (YAVP)">win</abbr> or <abbr title="Yet Another Sad Death">YASD</abbr>), just <b>submit your morgue</b> on the Unique's challenge details webpage. To track your progress, click on your name on that last page mentioned.
 <br />
 <p>Feel free to submit a morgue as soon as you've improved your score. Your best 2 runs for a specific challenge will be kept in the website's database.</p>
+<h2>Q&A</h2>
+<h3>Need more help than what's on the challenge details' page?</h3>
+<p>Ask all your questions to the Crawl Cosplay community on our <a href="https://discord.gg/WdbyURBcYp" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18">. Our @VIPs_Crawl_Cosplay group is there to help!</p>
 <h3>When should I start playing the weekly Crawl Cosplay Challenges?</h3>
 <p>You can start whenever but once you have 4 wins with one in each of the 4 playstyles, you'll likely feel ready to <b>compete</b> at an intermediate level in the weekly <a href="/ccc_home">Crawl Cosplay Challenge</a>.</p>
-<br>
-<h3>Miscellaneous</h3>
+<h2>Miscellaneous</h3>
 <ul> to view your morgue/dump press the # key in-game.</ul>
 <ul> add the following to you RCfile (online) or your init.txt file (offline) to have intermediate monsters show up with a yellow backgroung:</ul>
 <br>
-<pre><b>      tile_show_threat_levels = tough,nasty,unusual</b></pre>
+<pre><b>      tile_show_threat_levels = tough,nasty,unusual</b>      </pre>
 <p>Happy Crawling!</p>
 RoGGa, alkemann (our webdev) and the @VIPs_Crawl_Cosplay in discord
 <br>
