6b11a6c7520affbac0d00dfbd07148bffbd314c6
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-05 07:15:55 -0400

    Update head.html.php
    add  styles

--- content/layouts/tournament/head.html.php
+++ content/layouts/tournament/head.html.php
@@ -5,8 +5,52 @@
     <title><?=$page_title ?? 'Crawl Cosplay Trunk Tournament (CCTT)'?></title>
     <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
     <link rel="stylesheet" href="/css/tournament.css?v=<?=time()?>">
-<?php
 
+<style>
+	
+h1	{
+	font-size: 48px;
+	text-align: center;
+}
+
+.dropbtn {
+  background-color: #7D623C;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #7D623C;
+  min-width: 400px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #000000;}
+.dropdown:hover .dropdown-content {display: block;}
+.dropdown:hover .dropbtn {background-color: #7D623C;}
+
+</style>
+
+<?php
     if (isset($meta) && is_array($meta) && isset($meta['filename'])) {
         $meta += [
             'width' => 256,
