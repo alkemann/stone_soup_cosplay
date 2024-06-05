ece23a62a6255db8a5dda39f389af0616c3cef58
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-05 10:04:28 -0400

    update links to /cca/ files

--- content/pages/cca/about_cca.html.php
+++ content/pages/cca/about_cca.html.php
@@ -21,24 +21,24 @@ haven't won at least a couple of games.</p>
   Hover over a Unique's graphic to see their descriptions, and then click on one of them to open the Unique's challenge's details page.
 <table class="centerTable"><tr>
     <td><p><b>------------Melee Brutes------------</b></p><p></p>
-      <a href="/academy/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/uniques/Snorg.png" width="72" height="72"></a> 
-      <a href="/academy/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/uniques/Asterion.png" width="72" height="72"></a> 
-      <a href="/academy/achallengedetails?id=928" title="Wiglaf, the Mountain Dwarf Gladiator (MDGl)"><img src="/img/uniques/Wiglaf.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/uniques/Snorg.png" width="72" height="72"></a> 
+      <a href="/cca/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/uniques/Asterion.png" width="72" height="72"></a> 
+      <a href="/cca/achallengedetails?id=928" title="Wiglaf, the Mountain Dwarf Gladiator (MDGl)"><img src="/img/uniques/Wiglaf.png" width="72" height="72"></a>
     </td>
     <td><p><b>-----------Ranged Weapons-----------</b></p><p></p>
-      <a href="/academy/achallengedetails?id=929" title="Joseph, the Human Hunter (HuHu)"><img src="/img/uniques/Joseph.png" width="72" height="72"></a>
-      <a href="/academy/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/uniques/Sonja.png" width="72" height="72"></a>
-      <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/uniques/Vashnia.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=929" title="Joseph, the Human Hunter (HuHu)"><img src="/img/uniques/Joseph.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/uniques/Sonja.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/uniques/Vashnia.png" width="72" height="72"></a>
     </td>
     <td><p><b>---------------<abbr title="Melee mostly with some Magic">Hybrid</abbr>---------------</b></p><p></p>
-      <a href="/academy/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/uniques/Grum.png" width="72" height="72"></a>
-      <a href="/academy/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/uniques/Ilsuiw_water.png" width="72" height="72"></a>
-      <a href="/academy/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/uniques/Jory.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/uniques/Grum.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/uniques/Ilsuiw_water.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/uniques/Jory.png" width="72" height="72"></a>
     </td>
     <td><p><b>----------Mostly Magic----------</b></p>
-      <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/uniques/Tiamat.gif" width="48" height="72"></a> 
-      <a href="/academy/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/uniques/Fannar.png" width="72" height="72"></a> 
-      <a href="/academy/achallengedetails?id=937" title="Jorgrun, the Mountain Dwarf Earth Elementalist (MDEE)"><img src="/img/uniques/Jorgrun.png" width="72" height="72"></a>
+      <a href="/cca/achallengedetails?id=935" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/uniques/Tiamat.gif" width="48" height="72"></a> 
+      <a href="/cca/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/uniques/Fannar.png" width="72" height="72"></a> 
+      <a href="/cca/achallengedetails?id=937" title="Jorgrun, the Mountain Dwarf Earth Elementalist (MDEE)"><img src="/img/uniques/Jorgrun.png" width="72" height="72"></a>
     </td>
 </table>
 <p>Each CCA challenge has 3 conducts and 2 bonuses. The 3 conducts are specific to getting you to the Lair and/or Orcish Mines entrance(s).</p>
--- content/pages/cca/cca.html.php
+++ content/pages/cca/cca.html.php
@@ -61,7 +61,7 @@ use app\models\{Challenge, Submission, Player};
 </div>
 
 <h3>Ready?</h3>
-Want to know more before jumping in? Read the <a href="/academy/about_cca">About CCA</a> webpage.
+Want to know more before jumping in? Read the <a href="/cca/about_cca">About CCA</a> webpage.
 <h3>Set...</h3>
 If you have never won a game, your first Academy goal is to play one of the <b>"Melee Brutes"</b> listed below since they are considered to be the
 <b><i>easier</i></b> combos of the easiest category.
@@ -69,24 +69,24 @@ If you have never won a game, your first Academy goal is to play one of the <b>"
 Just click on one of the Uniques for the challenge's details listing the suggested 3 conducts and 2 bonuses.
 <h4><b>Melee Brutes</b></h4>
   <p>Focus primarily on the Strength attribute, prefer to wear heavy armor, and typically spend most of their time in melee combat. Skills to focus on are your primary weapon, fighting, armour, shields, throwing, evocations, and invocations (if applicable).</p>
-  <p> <a href="/academy/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/uniques/Snorg.png" width="48" height="48"></a> 
-      <a href="/academy/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/uniques/Asterion.png" width="48" height="48"></a> 
-      <a href="/academy/achallengedetails?id=928" title="Wiglaf, the Mountain Dwarf Gladiator (MDGl)"><img src="/img/uniques/Wiglaf.png" width="48" height="48"></a></p>
+  <p> <a href="/cca/achallengedetails?id=926" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/uniques/Snorg.png" width="48" height="48"></a> 
+      <a href="/cca/achallengedetails?id=927" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/uniques/Asterion.png" width="48" height="48"></a> 
+      <a href="/cca/achallengedetails?id=928" title="Wiglaf, the Mountain Dwarf Gladiator (MDGl)"><img src="/img/uniques/Wiglaf.png" width="48" height="48"></a></p>
 <h4><b>Ranged Weapons</b></h4>
   <p>Focus primarily on the Dexterity attribute, prefer to wear medium armor, and can fight up close or at range. Skills to focus on are your primary weapon, fighting, dodging, stealth, hexes, evocations, and invocations (if applicable).</p>
-  <p> <a href="/academy/achallengedetails?id=929" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/uniques/Joseph.png" width="48" height="48"></a>
-      <a href="/academy/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/uniques/Sonja.png" width="48" height="48"></a>
-      <a href="/academy/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/uniques/Vashnia.png" width="48" height="48"></a> </p>
+  <p> <a href="/cca/achallengedetails?id=929" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/uniques/Joseph.png" width="48" height="48"></a>
+      <a href="/cca/achallengedetails?id=930" title="Sonja, the Kobole Brigan (KoBr)"><img src="/img/uniques/Sonja.png" width="48" height="48"></a>
+      <a href="/cca/achallengedetails?id=931" title="Vashnia, the Naga Hunter (NaHu)"><img src="/img/uniques/Vashnia.png" width="48" height="48"></a> </p>
 <h4><b>Hybrid: Melee with some Magic</b></h4>
   <p>An assortment of less straightforward builds including shapeshifter and summoner/necromancer.</p>
-  <p> <a href="/academy/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/uniques/Grum.png" width="48" height="48"></a> 
-      <a href="/academy/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/uniques/Ilsuiw_water.png" width="48" height="48"></a> 
-      <a href="/academy/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/uniques/Jory.png" width="48" height="48"></a></p>
+  <p> <a href="/cca/achallengedetails?id=932" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/uniques/Grum.png" width="48" height="48"></a> 
+      <a href="/cca/achallengedetails?id=933" title="Ilsuiw, the Merfolk Warper (MfWr)"><img src="/img/uniques/Ilsuiw_water.png" width="48" height="48"></a> 
+      <a href="/cca/achallengedetails?id=934" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/uniques/Jory.png" width="48" height="48"></a></p>
 <h4><b>Mages: Mostly Magic</b></h4>
   <p>Focus primarily on the Intelligence attribute, prefer to wear light armor (robes in particular), and often but not always fight at long range. Skills to focus on are roughly two magic school(s), spellcasting, fighting, stealth, evocations, and invocations (if applicable).</p>
-  <p> <a href="/academy/achallengedetails?id=935" title="Tiamat, the Draconian Hedge Wizard (DrHW)"><img src="/img/uniques/Tiamat.gif" width="36" height="48"></a>
-      <a href="/academy/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/uniques/Fannar.png" width="48" height="48"></a> 
-      <a href="/academy/achallengedetails?id=937" title="Jorgrun, the Mountain Dwarf Earth Elementalist (MDEE)"><img src="/img/uniques/Jorgrun.png" width="48" height="48"></a></p>
+  <p> <a href="/cca/achallengedetails?id=935" title="Tiamat, the Draconian Hedge Wizard (DrHW)"><img src="/img/uniques/Tiamat.gif" width="36" height="48"></a>
+      <a href="/cca/achallengedetails?id=936" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/uniques/Fannar.png" width="48" height="48"></a> 
+      <a href="/cca/achallengedetails?id=937" title="Jorgrun, the Mountain Dwarf Earth Elementalist (MDEE)"><img src="/img/uniques/Jorgrun.png" width="48" height="48"></a></p>
 <h3>GO!</h3>
 Play a game of your favourite DCSS Uniques and once you have completed one of your better games, <b>Submit your Morgue</b> on the challenge's detail page and a moderator will approve it.
 Your best 2 runs for each challenge will be kept in the website's database...so submit as often as you like.
