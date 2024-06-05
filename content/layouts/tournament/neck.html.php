550b8b946751842ed91d6449be3d2923c3cde7ca
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-05 07:14:13 -0400

    Update /tournament/neck.html.php
    copy over the dropdown menu

--- content/layouts/tournament/neck.html.php
+++ content/layouts/tournament/neck.html.php
@@ -1,31 +1,35 @@
 <div class="page information">
-    <div class="toplinks">
-	     <a href="/">Crawl Cosplay Home</a>
-	   | <a href="/history">All CCC Challenges</a>
-	   | <a href="/about_ccc">About CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-	   | <a href="/ccc_home">Weekly active CCC</a>&nbsp&nbsp&nbsp&nbsp&nbsp
-    	<br>
-	   | <a href="/tournament/about">About CCTT</a>&nbsp&nbsp&nbsp&nbsp&nbsp
-	   | <a href="/tournament/home">Tournament (CCTT)</a> NEW!
-    	<br>
-	   | <a href="/recent">All Submissions</a>
-	   - To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>&nbsp&nbsp&nbsp&nbsp
-	 <br>
-        <?php if ($this->request->session('admin')) : ?>
-               	Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
-        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
-    		| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
-        	| <a href="/logout">Logout</a>
-	<?php else : ?>
-		<a href="/backoffice">Admin</a>
-	<?php endif; ?>
-    </div>
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">Menu</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/">Crawl Cosplay Home</a>
+			<a href="/academy">Crawl Cosplay Academy (CCA) - NEW!</a>
+			<a href="/academy/about_cca"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCA</a>
+			<a href="/ccc_home">Crawl Cosplay Challenge (CCC)</a>
+			<a href="/about_ccc"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCC</a>
+			<a href="/history"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; all CCC challenges</a>
+			<a href="/tournament/home">Crawl Cosplay Trunk Tournament (CCTT) - NEW!</a>
+			<a href="/tournament/about"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; About CCTT</a>
+		</div>
+	</div>
+
+    <div class="toplinks"><br></div>
     <div class="page_content">
         <div class="heading">
-            <h1><a href="/tournament/home"><img src="/img/stone_soup_icon-32x32.png">Crawl Cosplay Trunk Tournament<img src="/img/stone_soup_icon-32x32.png"></a></h1>
-            <div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></div>
+            <h1><<img src="/img/stone_soup_icon-32x32.png"> Crawl Cosplay Trunk Tournament <img src="/img/stone_soup_icon-32x32.png"></h1>
+            <div class="fineprint"><center>Come chat with us on our <a href="https://discord.gg/WdbyURBcYp" target="_blank">Discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
         </div>
-        <br />
+	<div class="fineprint" align="right">
+   		To be Moderated: <?php echo app\models\Submission::getNumberOfUnscoredSubmissions() ?>
+		<?php if ($this->request->session('admin')) : ?>
+        		| Subs <a href="/admin/submissions/list">Official</a> <a href="/admin/submissions/moderate">Moderate</a> <a href="/admin/submissions/add">New</a>
+	        	| Players <a href="/admin/players/list">List</a> <a href="/admin/players/add">New</a>
+    			| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
+	        	| <a href="/logout">Logout</a>
+		<?php else : ?>
+          		| <a href="/backoffice">Admin</a>
+		<?php endif; ?>
+	</div>
         <img src="/img/HR-right.png"><br />
         <div class="content" onclick="window.location = '/dismiss';">
         <?php if ($msg = $this->request->session()->get('message')) : ?>
