5af9e45bb7b3a1e6d0c4c6544df3c35a6de5112c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-06 17:53:20 -0400

    Update player.html.php

--- content/pages/player.html.php
+++ content/pages/player.html.php
@@ -246,9 +246,10 @@ if ($acadamy_sumissions) :
     <?php
         $r = 0;
         $set_key = 0;
-        echo '<tr class="'<?=$r++%2==0?'odd':'even'?>'">';
-        echo '<th>CCA</th>';
-        echo '<td><?=$board[$set_key]['total']?> <?=$board[$set_key]['stars']?><span class="star">&#9733;</span></td>';
+    ?>
+        <tr class="'<?=$r++%2==0?'odd':'even'?>'">
+        <th>CCA</th>
+        <td><?=$board[$set_key]['total']?> <?=$board[$set_key]['stars']?><span class="star">&#9733;</span></td>
         <?php for ($i=1; $i <= 5; $i++) : ?>
             <?php
                 $sub = $board[$set_key][$i] ?? false;
