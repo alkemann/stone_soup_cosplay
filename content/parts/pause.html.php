6f52eb50941c2556c924d3000eb726b47c38c26f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-06 07:29:19 -0400

    Update pause.html.php

--- content/parts/pause.html.php
+++ content/parts/pause.html.php
@@ -6,4 +6,4 @@
 */ ?>
 
 <h3>No currently active challenges</h3>
-<p>Maybe take a look at the <a href="/history">Challenge History</a>?</p>
+<p>Maybe take a look at <a href="/ccc/all_ccc_history">all CCC challenge</a>?</p>
