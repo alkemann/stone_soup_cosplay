068e462aaaccf894e2374e2953a065bd73a158f8
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-06 06:31:45 -0400

    Update submit_cca.html.php
    not sure if this will fix it!

--- content/pages/cca/submit_cca.html.php
+++ content/pages/cca/submit_cca.html.php
@@ -17,7 +17,7 @@ if (!$cha) {
 
 if ($cha && $cha->setnr > 30) {
     if ($cha->active) {
-        $this->layout = 'tournament';
+        $this->layout = 'cca';
     } else {
         return $this->request->redirect('/');
     }
