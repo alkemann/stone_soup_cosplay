8ed5d278a88ccbd5c9abd855d1fdddbd45636126
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-06 06:14:50 -0400

    Update cca.html.php

--- content/pages/cca/cca.html.php
+++ content/pages/cca/cca.html.php
@@ -24,7 +24,7 @@ use app\models\{Challenge, Submission, Player};
 				$made_seperator = true; // only make one seperator if multiple bonuses
 			}
 			echo '<th>'; 	// . $e($c->week) . '. ';
-			<td><?php if ($c->icon):?><img src="<?=$e($c->icon)?>" style="height: 2.5em" /><?php endif; ?> <b><a href="/cca/achallengedetails?id=<?=$e($c->id)?>"><?=$e($c->name)?></a></b></td>
+			echo '<td><?php if ($c->icon):?><img src="<?=$e($c->icon)?>" style="height: 2.5em" /><?php endif; ?> <b><a href="/cca/achallengedetails?id=<?=$e($c->id)?>"><?=$e($c->name)?></a></b></td>'
 			echo "</th>";
 		}
 		?>
