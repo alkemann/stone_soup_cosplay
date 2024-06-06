52741d890c97366f7017b328d503461e2749761e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2024-06-06 06:36:27 -0400

    Update challengedetails.html.php

--- content/pages/ccc/challengedetails.html.php
+++ content/pages/ccc/challengedetails.html.php
@@ -30,9 +30,9 @@ $this->setData("meta", ['filename' => $cha->icon]);
 <p style="font-style: italic; color: #777;"><?=$e($cha->description)?></p>
 <p>
 	<?php if ($cha->active) : ?>
-	<a href="/submit">Submit a run</a> 
+	<a href="/ccc/submit_ccc">Submit a run</a> 
 	<?php else: ?>
-	<a href="/submit?id=<?=$cha->id?>">Submit a run</a> 
+	<a href="/ccc/submit_ccc?id=<?=$cha->id?>">Submit a run</a> 
 	<?php endif;?>
 	<?php if ($cha->wiki): ?> |
 	<a href="<?=$e($cha->wiki)?>">Wiki page</a><?php endif; ?>
@@ -123,6 +123,4 @@ $this->setData("meta", ['filename' => $cha->icon]);
 	<li>Win the game.
 </ul>
 <p class="info">The main way to score points. +5 points each, and can be done in any order.</p>
-
-
 </div>
